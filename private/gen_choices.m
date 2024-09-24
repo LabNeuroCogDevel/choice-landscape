21333dcb86a69b3781c31d13396d08d98aa0466f
WillForan <willforan@gmail.com>
2024-09-24 14:15:03 -0400

    ✏️ ntrial not ntrials

--- private/gen_choices.m
+++ private/gen_choices.m
@@ -1,4 +1,4 @@
-function block_choices = gen_choices(ntrials)
+function block_choices = gen_choices(ntrial)
   % ntrial=36;
   choices = [ 1 2; 2 3; 1 3];
   block_choices = Shuffle(repmat(choices,ntrial/length(choices),1),2);
