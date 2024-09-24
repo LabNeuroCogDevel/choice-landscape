865291a463527e5ae8b11c4b5e1a8a9c5f612079
kylemrupp <kyle.m.rupp@gmail.com>
2024-09-24 14:21:04 -0400

    typo

--- private/gen_choices.m
+++ private/gen_choices.m
@@ -1,7 +1,7 @@
 function block_choices = gen_choices(ntrial)
   % ntrial=36;
   choices = [ 1 2; 2 3; 1 3];
-  block_choices = Shuffle(repmat(choices,ntrials/length(choices),1),2);
+  block_choices = Shuffle(repmat(choices,ntrial/length(choices),1),2);
 end
 %! block_choices = gen_choices(36);
 %! assert(all(size(block_choices) == [36     2]))
